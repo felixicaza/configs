7d2f908ac493bb9d13cde6d76e17011d41c1e02b
Felix Icaza <fx.joliett17@gmail.com>
2026-09-19 06:05:04 -0600

    fix(eslint): update and optimize json config

--- packages/eslint-config/src/configs/json.ts
+++ packages/eslint-config/src/configs/json.ts
@@ -3,23 +3,47 @@ import type { Linter } from 'eslint'
 import { configs as configJsonc } from 'eslint-plugin-jsonc'
 
 export const json: Linter.Config[] = [
-  ...configJsonc['flat/recommended-with-json'],
-  ...configJsonc['flat/recommended-with-jsonc'],
-  ...configJsonc['flat/recommended-with-json5'],
+  ...configJsonc.base,
   {
-    name: 'felixicaza/json',
+    name: 'felixicaza/json/global',
     files: ['**/*.json', '**/*.jsonc', '**/*.json5'],
     rules: {
+      'jsonc/comma-dangle': ['error', 'never'],
       'jsonc/indent': ['error', 2],
       'jsonc/key-spacing': ['error', { beforeColon: false, afterColon: true }],
-      'jsonc/comma-dangle': ['error', 'never']
+      'jsonc/no-bigint-literals': 'error',
+      'jsonc/no-binary-expression': 'error',
+      'jsonc/no-binary-numeric-literals': 'error',
+      'jsonc/no-dupe-keys': 'error',
+      'jsonc/no-escape-sequence-in-identifier': 'error',
+      'jsonc/no-floating-decimal': 'error',
+      'jsonc/no-hexadecimal-numeric-literals': 'error',
+      'jsonc/no-infinity': 'error',
+      'jsonc/no-irregular-whitespace': 'error',
+      'jsonc/no-multi-str': 'error',
+      'jsonc/no-nan': 'error',
+      'jsonc/no-number-props': 'error',
+      'jsonc/no-numeric-separators': 'error',
+      'jsonc/no-octal-numeric-literals': 'error',
+      'jsonc/no-octal': 'error',
+      'jsonc/no-parenthesized': 'error',
+      'jsonc/no-regexp-literals': 'error',
+      'jsonc/no-sparse-arrays': 'error',
+      'jsonc/no-template-literals': 'error',
+      'jsonc/no-undefined-value': 'error',
+      'jsonc/no-unicode-codepoint-escapes': 'error',
+      'jsonc/no-useless-escape': 'error',
+      'jsonc/quote-props': 'error',
+      'jsonc/quotes': 'error',
+      'jsonc/space-unary-ops': 'error',
+      'jsonc/valid-json-number': 'error'
     }
   },
   {
-    name: 'felixicaza/jsonc',
-    files: ['**/*.jsonc'],
+    name: 'felixicaza/json',
+    files: ['**/*.json'],
     rules: {
-      'jsonc/no-comments': 'off'
+      'jsonc/no-comments': 'error'
     }
   }
 ]
